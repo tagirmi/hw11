06002ce4751607164cd4c03b613fc5663c2f9907
tagirmi <tagirmi@gmail.com>
2018-10-31 21:55:57 +0300

    реализация async

--- async.cpp
+++ async.cpp
@@ -1,15 +1,25 @@
+#include <list>
+#include <memory>
+
 #include "async.h"
+#include "context.h"
 
 namespace async {
 
 handle_t connect(std::size_t bulk) {
-    return nullptr;
+  return hw11::ContextManager::instance().createContext(bulk);
 }
 
-void receive(handle_t handle, const char *data, std::size_t size) {
+void receive(handle_t handle, const char* data, std::size_t size)
+{
+  auto context = hw11::ContextManager::instance().findContext(handle);
+  if (context)
+    context->recieve(data, size);
 }
 
-void disconnect(handle_t handle) {
+void disconnect(handle_t handle)
+{
+  hw11::ContextManager::instance().destroyContext(handle);
 }
 
 }
